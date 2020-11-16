4d88d9a28d149cf9c784b279c070c0f8da9f8518
NUTTENS Florent <florent.nuttens@e-i.com>
2020-11-16 16:07:06 +0100

    solve 7_final exercises

--- lib/7_final.rb
+++ lib/7_final.rb
@@ -4,10 +4,24 @@
 
 # Créer un classe "Student" avec comme attribut un prénom et un tableau de note
 
-# Créer une méthode au sein de la classe 'Student' qui calcul la moyenne des notes du tableau de notes de l'élève 
+# Créer une méthode au sein de la classe 'Student' qui calcule la moyenne des notes du tableau de notes de l'élève 
 
 # Créer une méthode qui retourne une phrase indiquant la moyenne de l'élève
+class Student
+  attr_reader :first_name
+
+  def initialize(first_name, grades)
+    @first_name = first_name
+    @grades = grades
+  end
+
+  def get_average
+    @grades.sum / @grades.length
+  end
+end
 
 # Créer une instance de l'objet Student
+student = Student.new("Dabao", [15, 10, 8, 17, 12])
 
 # Afficher la phrase indiquant la moyenne de l'élève en une seule commande
+puts "L'élève #{student.first_name} a une moyenne de #{student.get_average}"
