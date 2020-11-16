185511a69d086eda6c3a3bc86ddd9107a69364a1
NUTTENS Florent <florent.nuttens@e-i.com>
2020-11-16 16:00:33 +0100

    solve 6_classes exercises

--- lib/6_classes.rb
+++ lib/6_classes.rb
@@ -9,12 +9,23 @@
 # Définir 2 attributs à la classe voiture : 
 #    - 'brand'
 #    - 'color'
+class Voiture
+  attr_accessor :color
+
+  def initialize(brand, color)
+    @brand = brand
+    @color = color
+  end
+end
 
 # 3 - Instancier un objet
 # Instancier un objet de la classe voiture dans une variable 'my_first_car' avec les attributs de votre première voiture
+my_first_car = Voiture.new("Peugeot", "gray")
 
 # 4 - Manipulation d'un objet : lecture
-# Afficher la couleur de votre première voiture 
+# Afficher la couleur de votre première voiture
+puts my_first_car.color
 
 # 5 - Manipulation d'un objet : modification
-# Modifier la couleur de votre première voiture par votre couleur préférée 
+# Modifier la couleur de votre première voiture par votre couleur préférée
+my_first_car.color = "pink"
